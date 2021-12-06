1fe1506b2a1142e3e4d2896c96003630f49b585a
giancarlo <giancarlosossam@gmail.com>
2021-12-06 15:00:41 -0500

    formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormularComponent } from './formular/formular.component';
+
+//angular
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formular/formular.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-formular',
+  templateUrl: './formular.component.html',
+  styleUrls: ['./formular.component.css']
+})
+export class FormularComponent implements OnInit {
+  selected = 'none';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
